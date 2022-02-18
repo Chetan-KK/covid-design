0fd6b7e657ef3ed2cf69d94c0c742cdc9ef2021d
Chetan <chetankhulage350@gmail.com>
2022-02-18 08:04:32 +0530

    added vaccine type for selection

--- js/signup.js
+++ js/signup.js
@@ -44,6 +44,7 @@ function close_nav() {
 
 const dose1date = document.querySelector(`.dosesDate .dose1`)
 const dose2date = document.querySelector(`.dosesDate .dose2`)
+const doseType = document.getElementById(`doseType`)
 
 const doses = document.getElementById(`doses`)
 resetDoses()
@@ -55,12 +56,14 @@ doses.addEventListener(`change`, () => {
         resetDoses()
         dose1date.removeAttribute(`disabled`)
         dose1date.setAttribute(`required`, `required`)
+        doseType.removeAttribute(`disabled`)
     }
     else if (doses.value == 2) {
         dose1date.removeAttribute(`disabled`)
         dose2date.removeAttribute(`disabled`)
         dose1date.setAttribute(`required`, `required`)
         dose2date.setAttribute(`required`, `required`)
+        doseType.removeAttribute(`disabled`)
     }
 })
 
@@ -69,5 +72,14 @@ function resetDoses() {
     dose2date.setAttribute(`disabled`, `disabled`)
     dose1date.removeAttribute(`required`, `required`)
     dose2date.removeAttribute(`required`, `required`)
+    doseType.setAttribute(`disabled`, `disabled`)
+    dose1date.value = null;
+    dose2date.value = null;
+    doseType.value = null;
+    const option = document.createElement(`option`)
+    option.setAttribute(`hidden`, `hiddin`)
+    option.setAttribute(`selected`, `selected`)
+    option.setAttribute(`disabled`, `disabled`)
+    option.innerText = `select vaccine type`
+    doseType.appendChild(option)
 }
-
